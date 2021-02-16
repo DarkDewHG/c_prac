c1cafdb5e07fd75d7a579f8deef9dc45e3ca852a
DarkDewHG <sukalkin01@mail.ru>
2021-02-16 22:56:11 +0300

    2.c

---
+++ var_1/2.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int flag,pid;
+int fd[2];
+
+void alrmHandler(int signo)
+{
+	signal(SIGALRM,alrmHandler);
+	if (flag)
+	{
+		kill(pid,SIGKILL);
+		_exit(0);
+	}
+	int ef = EOF;
+	write(fd[1],&ef,sizeof(int));
+	alarm(1);
+	flag=1;
+	wait(NULL);
+}
+
+void childHandler(int signo)
+{
+	signal(SIGCHLD,childHandler);
+	wait(NULL);
+	printf("prog ended");
+}
+
+
+
+int main(int argc, char** argv)
+{
+	flag=0;
+	char* word[] = {"alpha\n","beta\n","gamma\n","epsilon\n","tetta\n"};
+	signal(SIGCHLD,childHandler);
+	signal(SIGALRM,alrmHandler);
+	int i=0;
+	pipe(fd);
+	pid = fork();
+	if (pid==0)
+	{
+		dup2(fd[0],0);
+		close(fd[1]);
+		execvp(argv[1],argv+1);
+		perror(argv[1]);
+		fflush(stderr);
+		_exit(1);
+	}
+	close(fd[0]);
+	alarm(5);
+	char b = 54;
+	for(;;)
+	{
+		write(fd[1],word[i],strlen(word[i]));
+		i++;
+		if (i==5)
+			i=0;
+	}
+
+}
